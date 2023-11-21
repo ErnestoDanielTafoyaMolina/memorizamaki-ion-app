70ac8283b44e0790deb9569d6954c7ffaee70c26
ErnestoDanielTafoyaMolina <edaniimol@gmail.com>
2023-11-20 23:11:52 -0600

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
-
+  URL = environment.URL;
 }
